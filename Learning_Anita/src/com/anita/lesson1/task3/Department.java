0a967fd748d3e628dad8eab8ec9859fa7eba7140
AdamStan <adam15stan@gmail.com>
2022-03-07 20:05:11 +0100

    Task3a

--- Learning_Anita/src/com/anita/lesson1/task3/Department.java
+++ Learning_Anita/src/com/anita/lesson1/task3/Department.java
@@ -1,4 +1,46 @@
 package com.anita.lesson1.task3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Department {
+    private int id;
+    private String name;
+    private final List<Employee> employees = new ArrayList<>();
+
+    public Department() {
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- Learning_Anita/src/com/anita/lesson1/task3/Employee.java
+++ Learning_Anita/src/com/anita/lesson1/task3/Employee.java
@@ -1,4 +1,59 @@
 package com.anita.lesson1.task3;
 
 public class Employee {
+    private int id;
+    private String name;
+    private String lastName;
+    private Employee boss;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, String lastName) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Employee getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Employee boss) {
+        this.boss = boss;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", boss=" + boss +
+                '}';
+    }
 }
